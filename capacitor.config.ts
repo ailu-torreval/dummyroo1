0c5ff6c822355fe7da32c7c5c848b5e147ca1673
ailin <ailu.torreval@gmail.com>
2022-09-06 12:08:42 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dummyroo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
